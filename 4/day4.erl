f1f388383785b6fee14443a5a101118c17946783
Steven Oud <soud@protonmail.com>
2015-12-12 13:39:07 +0100

    day 4

---
+++ 4/day4.erl
@@ -0,0 +1,45 @@
+%%% -- Part One --
+%%% Santa needs help mining some AdventCoins (very similar to bitcoins) to use
+%%% as gifts for all the economically forward-thinking little girls and boys.
+%%%
+%%% To do this, he needs to find MD5 hashes which, in hexadecimal, start with at
+%%% least five zeroes. The input to the MD5 hash is some secret key (your puzzle
+%%% input, given below) followed by a number in decimal. To mine AdventCoins,
+%%% you must find Santa the lowest positive number (no leading zeroes: 1, 2, 3,
+%%% ...) that produces such a hash.
+%%%
+%%% -- Part Two --
+%%% Now find one that starts with six zeroes.
+
+-module(day4).
+-export([main/0, part1/1, part2/1]).
+
+main() ->
+    Contents = io:get_line("Input> ") -- "\n",
+    io:fwrite("Part 1: ~p~n", [part1(Contents)]),
+    io:fwrite("Part 2: ~p~n", [part2(Contents)]).
+
+part1(Contents) -> mine_advent_coins(Contents, "00000").
+part2(Contents) -> mine_advent_coins(Contents, "000000").
+
+mine_advent_coins(Input, Match) ->
+    mine_advent_coins(Input, 1, Match).
+mine_advent_coins(Input, C, Match) ->
+    case string:str(md5_hex([Input|integer_to_list(C)]), Match) of
+        1 -> C;
+        _ -> mine_advent_coins(Input, C + 1, Match)
+    end.
+
+md5_hex(Input) ->
+	lists:flatten(list_to_hex(binary_to_list(erlang:md5(Input)))).
+
+list_to_hex(L) ->
+	lists:map(fun(X) -> int_to_hex(X) end, L).
+
+int_to_hex(N) when N < 256 ->
+	[hex(N div 16), hex(N rem 16)].
+
+hex(N) when N < 10 ->
+	$0+N;
+hex(N) when N >= 10, N < 16 ->
+	$a + (N - 10).
